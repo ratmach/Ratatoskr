90f72a8df86e36fef6ea00d4986ea8b728366cee
Rshalika <rshal13@freeuni.edu.ge>
2017-05-17 07:02:57 +0400

    refactor

--- Ratatoskr/anotations.py
+++ Ratatoskr/anotations.py
@@ -1,13 +1,6 @@
 def handleRequest(method=None):
-    registry = {}
-
     def registrar(func):
-        registry["s"] = func
         func.__ratamethod__ = method
         return func
-
-    registrar.all = registry
     return registrar
-
-
 handler = handleRequest
